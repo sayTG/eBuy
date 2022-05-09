09f3f158ea270c8cb33a6e67ece90566d1e0d5fb
sayTG <busarithienkie@gmail.com>
2022-05-09 23:33:21 +0100

    refactor: cart controller

--- eBuy/eBuy/Controllers/CartController.cs
+++ eBuy/eBuy/Controllers/CartController.cs
@@ -33,7 +33,7 @@ namespace eBuy.Controllers
             else
                 return View("~/Views/500.cshtml");
         }
-        public IActionResult ViewCart()
+        public IActionResult Index()
         {
             return View();
         }
